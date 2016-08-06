47d2ec719b8abd10363bb0bb4a47a82f85e8572f
Stephen Maitland <s.maitland@me.com>
2016-08-06 11:58:46 +0100

    Initial setup

---
+++ Source/Building_Escape/Building_EscapeGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "Building_EscapeGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDING_ESCAPE_API ABuilding_EscapeGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
